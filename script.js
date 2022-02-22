e00f094f4709d66dffac97a9a7c7968e439b3c68
Jake Smiley <hello@jakesmiley.com>
2022-02-22 15:43:21 -0800

    begin class implementation

--- script.js
+++ script.js
@@ -1,61 +1,87 @@
 "use strict";
-
 const addMarker = document.querySelector(".btn--add-marker");
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// get position
-let coords;
-let addingNewMarker;
-var map;
 
-const options = {
-  maximumAge: 0,
-  enableHighAccuracy: true,
-  timeout: 5000,
-};
+class App {
+  #map;
+  #mapEvent;
+  #addingNewMarker;
+  constructor() {
+    this._getPosition();
+    addMarker.addEventListener("click", this._toggleAddingNewMarker.bind(this));
+    // this._loadMap();
+  }
+  _getPosition() {
+    let coords;
 
-const success = function (position) {
-  coords = [position.coords.latitude, position.coords.longitude];
-  map = L.map("map", { zoomControl: false }).setView(coords, 11);
+    const options = {
+      maximumAge: 0,
+      enableHighAccuracy: true,
+      timeout: 5000,
+    };
 
-  let myIcon = L.icon({
-    iconUrl: "/lib/images/purple-pin.png",
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  });
+    const success = function (position) {
+      console.log("success...loading map");
+      this._loadMap(position);
+    };
 
-  L.marker(coords, { icon: myIcon })
-    .addTo(map)
-    .bindPopup("New Job")
-    .openPopup();
-  L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-  new L.control.zoom({ position: "topright" }).addTo(map);
+    const failure = function (error) {
+      console.log(`Failed to get position - ${error}`);
+    };
 
-  map.on("click", function (e) {
-    const clickCoords = [e.latlng.lat, e.latlng.lng];
-    if (addingNewMarker) {
+    navigator.geolocation.getCurrentPosition(
+      success.bind(this),
+      failure,
+      options
+    );
+  }
+
+  _loadMap(position) {
+    this.coords = [position.coords.latitude, position.coords.longitude];
+    this.#map = L.map("map", { zoomControl: false }).setView(this.coords, 11);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+    new L.control.zoom({ position: "topright" }).addTo(this.#map);
+
+    // Not sure how to fix this...
+    // Problem: Can't send event and bind this on _renderMarker at the same time.
+    // Solution: Weird helper function below
+    const markerHelper = function (e) {
+      this._renderMarker(e);
+    };
+    this.#map.addEventListener("click", markerHelper.bind(this));
+  }
+
+  _toggleAddingNewMarker() {
+    this.#addingNewMarker = !this.#addingNewMarker;
+    console.log("addingNewMarker = " + this.#addingNewMarker);
+    document.querySelector("#map").classList.toggle("adding-new-marker");
+  }
+
+  _renderMarker(event) {
+    const myIcon = L.icon({
+      iconUrl: "/lib/images/purple-pin.png",
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+      popupAnchor: [0, -40],
+    });
+    const clickCoords = [event.latlng.lat, event.latlng.lng];
+
+    if (this.#addingNewMarker) {
       L.marker(clickCoords, { icon: myIcon })
-        .addTo(map)
+        .addTo(this.#map)
         .bindPopup("Another New Job")
         .openPopup();
-      addingNewMarker = false;
+      this.#addingNewMarker = false;
       document.querySelector("#map").classList.remove("adding-new-marker");
       // show new job form
     }
-  });
-};
+  }
+}
 
-const failure = function (error) {
-  console.log(`Error ${error}`);
-};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// get position
 
-navigator.geolocation.getCurrentPosition(success, failure, options);
-
-addMarker.addEventListener("click", function () {
-  addingNewMarker = !addingNewMarker;
-  console.log("addingNewMarker = " + addingNewMarker);
-  document.querySelector("#map").classList.toggle("adding-new-marker");
-});
+const app = new App();
